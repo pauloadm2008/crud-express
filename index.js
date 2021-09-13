db232e7f2b7d7d898007eb40ea9542dfffcb7b6b
pauloadm2008 <pauloadm2008@gmail.com>
2021-09-13 18:20:16 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { v4 } = require("uuid")
+
+const app = express();
+
+app.use(express.json())
+
+//METODOS HTTP 
+//GET - LISTAR
+//POST - CRIAR
+//PUT - ATUALIZAR
+//DELETE - DELETAR
+
+const usuarios = []
+
+//http://localhost:3333/users
+app.post('/users', (request, response) => {
+//aqui eu recebo os dados do front-end
+  const {name, lastname, phone, sexo} = request.body;
+//aqui eu armazeno os dados recebidos em uma variavel
+  const usuario = { id: v4(), name, lastname, phone, sexo}
+
+//eu publico os dados da variavel dentro do meu vetor/banco de dados
+usuarios.push(usuario)
+
+return response.json(usuario)
+})
+
+
+
+app.listen(3333)  //  http://localhost:3333
