84d989f2d314d114c40a2986383ad88756629fd7
pauloadm2008 <pauloadm2008@gmail.com>
2021-09-13 18:42:50 -0300

    configuracao inincial de crud usando express

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-const express = require("express");
-const { v4 } = require("uuid")
+const express = require("express")
+//estou importando a minha biblioteca
 
-const app = express();
+const app = express()
+//atribuindo uma funcao para a varialvel app
 
 app.use(express.json())
+//determinando json como formato de dados
 
+app.listen(3333)
+//abriu uma porta para o servidor
 
-
-const usuarios = []
-
-//http://localhost:3333/users
-app.post('/users', (request, response) => {
-//aqui eu recebo os dados do front-end
-  const {name, lastname, phone, sexo} = request.body;
-//aqui eu armazeno os dados recebidos em uma variavel
-  const usuario = { id: v4(), name, lastname, phone, sexo}
-
-//eu publico os dados da variavel dentro do meu vetor/banco de dados
-usuarios.push(usuario)
-
-return response.json(usuario)
-})
-
-
-
-app.listen(3333)  //  http://localhost:3333
