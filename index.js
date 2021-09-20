530f2fa8db7362617dc99ce0557e01d20dcbea80
pauloadm2008 <pauloadm2008@gmail.com>
2021-09-20 19:01:25 -0300

    implementacao do get e do put na funcao

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { request, response } = require("express")
 const express = require("express")
 //estou importando framework(conjunto de funcionalidades de http) express
+
+const {v4} = require("uuid")
 
 const app = express()
 //atribuindo uma funcao para a varialvel app
@@ -10,9 +13,13 @@ app.use(express.json())
 const usuario = []
 //construindo um array com a finalidade de armazenar os usuários
 
+app.get("/users", (request, response) => { //estou listando com o comando get
+    return response.json(usuario)
+})
+
 app.post("/users", (request, response) => {
     const {nome, email, senha} = request.body //aqui eu recebo as informacoes do meu front end
-    const novousuario = {nome, email, senha}//aqui eu persisto(tenho nas minhas maos as informacoes) para salvalas no meu vetor
+    const novousuario = { id: v4(), nome, email, senha}//aqui eu persisto(tenho nas minhas maos as informacoes) para salvalas no meu vetor
 
     usuario.push(novousuario)//publicar a variavel dentro do vetor
 
@@ -20,6 +27,14 @@ app.post("/users", (request, response) => {
 
 })
 //estou criando um funcao para criar usuarios
+
+app.put("/users/:id", (request, response) => {
+    const {id} = request.params //usado para enviar o id como parametro para identificalo dentro da minha base de dados.
+    const {nome, email, senha} = request.body //alem de criar o request.body tambem serve para atualizar.
+    })
+
+
+
 
 app.listen(3333)
 //abriu uma porta para o servidor
